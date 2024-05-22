d3cb0118c9e7c584bffcacdae093b28f886db2c7
tay <phuonggteyy@gmail.com>
2024-05-22 19:26:07 +0700

    Game Play Analysis I

---
+++ GamePlayAnalysisI.sql
@@ -0,0 +1,17 @@
+create database game_play_analysis_I;
+use game_play_analysis_I;
+
+create table Activity (
+	player_id int,
+    device_id int,
+    event_date date,
+    games_played int,
+    primary key (player_id, event_date)
+);
+
+insert into Activity values
+(1, 2, "2016-03-01", 5),
+(1, 2, "2016-05-02", 6),
+(2, 3, "2017-06-25", 1),
+(3, 1, "2016-03-02", 0),
+(3, 4, "2018-07-03", 5);
